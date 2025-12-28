f45d693c27f407f251b67b50c3e025a3964918a7
arvam <arvamartin02@gmail.com>
2025-12-28 16:28:14 +0100

    Modified Allure report settings

--- src/main/java/framework/core/Browser.java
+++ src/main/java/framework/core/Browser.java
@@ -18,19 +18,22 @@ public class Browser {
         return driver;
     }
 
-    private static void initializeDriver(){
+    private static void initializeDriver() {
         ChromeOptions options = new ChromeOptions();
 
         Map<String, Object> prefs = new HashMap<>();
-
         prefs.put("credentials_enable_service", false);
         prefs.put("profile.password_manager_enabled", false);
         prefs.put("profile.password_manager_leak_detection", false);
-
         options.setExperimentalOption("prefs", prefs);
 
-        driver = new ChromeDriver(options);
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
 
+        driver = new ChromeDriver(options);
     }
 
     public static void quitDriver() {
